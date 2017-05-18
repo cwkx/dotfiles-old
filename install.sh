a7d0e5f4eb734a021c706c1ae3e817d4b789fb58
Chris Willcocks <christopher.g.willcocks@durham.ac.uk>
2017-05-18 12:30:08 +0000

    vimb config

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ pacaur -S wget curl git binutils oh-my-zsh-git gst-libav gst-plugins-good vimb
 
 # create paths that may not exist
 mkdir -p ~/.vim/colors 
+mkdir -p ~/.config/vimb
 
 # create symlinks
 ln -s ~/repos/dotfiles/.vimrc		~/.vimrc
@@ -13,6 +14,7 @@ ln -s ~/repos/dotfiles/.zshrc		~/.zshrc
 ln -s ~/repos/dotfiles/.latexmkrc	~/.latexmkrc
 ln -s ~/repos/dotfiles/.xinitrc		~/.xinitrc
 ln -s ~/repos/dotfiles/vim/cols.vim	~/.vim/colors
+ln -s ~/repos/dotfiles/vimb/config	~/.config/vimb
 
 # install vundle and vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
