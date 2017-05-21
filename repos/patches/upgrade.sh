6e2054d33e46e6eb9a3c3f7e6648aef0abe353ec
Chris Willcocks <chris.willcocks@gmail.com>
2017-05-21 23:21:26 +0100

    several changes

--- repos/patches/upgrade.sh
+++ repos/patches/upgrade.sh
@@ -10,4 +10,9 @@ cd ../st && git reset --hard HEAD && git clean -fxd
 git apply ../patches/st-config-20170519-0.7.diff
 make && sudo make install
 
+# dmenu
+cd ../dmenu && git reset --hard HEAD && git clean -fxd
+git apply ../patches/dmenu-config-20170521-4.7.diff
+make && sudo make install
+
 cd ..
