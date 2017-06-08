a64deaa5988270fbab4fc3d5dea6c6c728e238eb
Chris Willcocks <christopher.g.willcocks@durham.ac.uk>
2017-06-08 11:59:51 +0000

    added grid mode

--- repos/patches/upgrade.sh
+++ repos/patches/upgrade.sh
@@ -3,13 +3,14 @@
 # dwm
 cd ../dwm && git reset --hard HEAD && git clean -fxd
 git apply ../patches/dwm-config-20170519-6.1.diff
+git apply ../patches/dwm-gridmode-5.8.2.diff
 make && sudo make install
 
 # st
 cd ../st && git reset --hard HEAD && git clean -fxd
+git apply ../patches/st-config-20170519-0.7.diff
 git apply ../patches/st-scrollback-20170329-149c0d3.diff
 git apply ../patches/st-scrollback-mouse-20170427-5a10aca.diff
-git apply ../patches/st-config-20170519-0.7.diff
 make && sudo make install
 
 # dmenu
