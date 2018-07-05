279b4e626f6f43c642ba963cf81f515beac11101
Chris Willcocks <chris.willcocks@gmail.com>
2018-07-05 11:14:24 +0100

    updates

--- repos/patches/install.sh
+++ repos/patches/install.sh
@@ -5,9 +5,7 @@ pacaur -S wget curl git binutils oh-my-zsh-git gst-libav gst-plugins-good vimb
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-# install helper scripts and symlinks
-sudo cp web /usr/bin/
-
+pacman -S xorg-server xorg-xrdb
 fc-cache -f $font_dir
 
 # setup
